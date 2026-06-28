a335f78c230f938670cb2ae11fceafc8ae6540bb
compaorecheickmohamed17-beep <compaorecheickmohamed17@gmail.com>
2026-06-28 12:45:24 +0000

    4eme commit

--- src/Calculatrice.java
+++ src/Calculatrice.java
@@ -17,6 +17,9 @@ public class Calculatrice {
     public double diviser(double a, double b) {
         return a / b;
     }
+    public double racineCarree(double a) {
+    return Math.sqrt(a);
+    }
 
     public static void main(String[] args) {
         Scanner clavier = new Scanner(System.in);
@@ -34,10 +37,12 @@ public class Calculatrice {
         double difference = calcul.soustraction(nombre1, nombre2);
         double produit = calcul.multiplication(nombre1, nombre2);
         double quotient = calcul.diviser(nombre1, nombre2);
+        double racine = calcul.racineCarree(nombre1);
 
         System.out.println("Addition : " + somme);
         System.out.println("Soustraction : " + difference);
         System.out.println("Multiplication : " + produit);
         System.out.println("Division : " + quotient);
+        System.out.println("Racine carree du premier nombre : " + racine);
     }
 }
