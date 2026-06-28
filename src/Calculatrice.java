b9609798010f84b4d14b670798cb93ec9d604586
Arsene-cloud <arsenerenikiema81@gmail.com>
2026-06-28 02:34:25 +0100

    1e commit

---
+++ src/Calculatrice.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Calculatrice {
+
+    public double addition(double a, double b) {
+        return a + b;
+    }
+    public double soustraction(double a, double b) {
+        return a - b;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner clavier = new Scanner(System.in);
+        Calculatrice calcul = new Calculatrice();
+
+        System.out.println("CALCULATRICE");
+
+        System.out.print("Entrez le premier nombre : ");
+        double nombre1 = clavier.nextDouble();
+
+        System.out.print("Entrez le deuxieme nombre : ");
+        double nombre2 = clavier.nextDouble();
+
+        double somme = calcul.addition(nombre1, nombre2);
+        double difference = calcul.soustraction(nombre1, nombre2);
+
+        System.out.println("Addition : " + nombre1 + " + " + nombre2 + " = " + somme);
+        System.out.println("Soustraction : " + nombre1 + " - " + nombre2 + " = " + difference);
+
+        clavier.close();
+    }
+}
